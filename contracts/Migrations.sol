42ad1b13331bfa8fddeecb270542e8ddeaf61734
Linfeng Qian <thewawar@gmail.com>
2020-08-01 18:02:12 +0800

    First commit

---
+++ contracts/Migrations.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.5.0;
+
+import "@ensdomains/ens/contracts/ENSRegistry.sol";
+/* import "@ensdomains/ens/contracts/FIFSRegistrar.sol"; */
+import "@ensdomains/ens/contracts/ReverseRegistrar.sol";
+import "@ensdomains/resolver/contracts/PublicResolver.sol";
+import "@ensdomains/ethregistrar/contracts/BaseRegistrarImplementation.sol";
+import "@ensdomains/ethregistrar/contracts/ETHRegistrarController.sol";
+
+contract DummyPriceOracle is PriceOracle {
+  constructor() public {}
+  /**
+   * @dev Returns the price to register or renew a name.
+   * @param name The name being registered or renewed.
+   * @param expires When the name presently expires (0 if this is a new registration).
+   * @param duration How long the name is being registered or extended for, in seconds.
+   * @return The price of this renewal or registration, in wei.
+   */
+  function price(string calldata name, uint expires, uint duration) external view returns(uint) {
+    return 10;
+  }
+}
+
+// Construct a set of test ENS contracts.
+contract TestDependencies {
+  bytes32 constant TLD_LABEL = keccak256("eth");
+  bytes32 constant RESOLVER_LABEL = keccak256("resolver");
+  bytes32 constant REVERSE_REGISTRAR_LABEL = keccak256("reverse");
+  bytes32 constant ADDR_LABEL = keccak256("addr");
+
+  ENSRegistry public ens;
+  BaseRegistrarImplementation public ethRegistrar;
+  ReverseRegistrar public reverseRegistrar;
+  PublicResolver public publicResolver;
+
+  event AllAddresses(address ens,
+                     address ethRegistrar,
+                     address reverseRegistrar,
+                     address publicResolver);
+
+  function namehash(bytes32 node, bytes32 label) public pure returns (bytes32) {
+    return keccak256(abi.encodePacked(node, label));
+  }
+
+  function getENSAddress() external view returns (address) {
+    return address(ens);
+  }
+  function getRegistrarAddress() external view returns (address) {
+    return address(ethRegistrar);
+  }
+  function getResolverAddress() external view returns (address) {
+    return address(publicResolver);
+  }
+  function getReverseAddress() external view returns (address) {
+    return address(reverseRegistrar);
+  }
+
+  constructor() public {
+    ens = new ENSRegistry();
+    publicResolver = new PublicResolver(ens);
+
+    // Set up the resolver
+    bytes32 resolverNode = namehash(bytes32(0), RESOLVER_LABEL);
+    /* bytes32 tldNode = namehash(bytes32(0), TLD_LABEL); */
+
+    ens.setSubnodeOwner(bytes32(0), RESOLVER_LABEL, address(this));
+
+    ens.setResolver(resolverNode, address(publicResolver));
+    publicResolver.setAddr(resolverNode, address(publicResolver));
+
+    /* ens.setResolver(tldNode, address(publicResolver)); */
+    /* publicResolver.setAddr(tldNode, address(publicResolver)); */
+
+    // Create a ETH registrar for the TLD
+    ethRegistrar = new BaseRegistrarImplementation(ens, namehash(bytes32(0), TLD_LABEL));
+    ens.setSubnodeOwner(bytes32(0), TLD_LABEL, address(ethRegistrar));
+
+    DummyPriceOracle prices = new DummyPriceOracle();
+    uint minCommitmentAge = 1;
+    uint maxCommitmentAge = 3600;
+    ETHRegistrarController controller = new ETHRegistrarController(ethRegistrar, PriceOracle(prices), minCommitmentAge, maxCommitmentAge);
+    ethRegistrar.addController(address(controller));
+
+    // Construct a new reverse registrar and point it at the public resolver
+    reverseRegistrar = new ReverseRegistrar(ens, NameResolver(address(publicResolver)));
+
+    // Set up the reverse registrar
+    ens.setSubnodeOwner(bytes32(0), REVERSE_REGISTRAR_LABEL, address(this));
+    ens.setSubnodeOwner(namehash(bytes32(0), REVERSE_REGISTRAR_LABEL), ADDR_LABEL, address(reverseRegistrar));
+
+    emit AllAddresses(address(ens), address(ethRegistrar), address(publicResolver), address(reverseRegistrar));
+  }
+}
---
+++ server.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import sys
+import subprocess
+import json
+from typing import Any, Dict, List, Union, NoReturn, Optional
+
+from flask import Flask, request
+from flask_jsonrpc import JSONRPC
+from flask_jsonrpc.exceptions import JSONRPCError
+from flask_cors import CORS
+
+sender = '0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7'
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+jsonrpc = JSONRPC(app, '/', enable_web_browsable_api=True)
+polyjuice_rpc_url = sys.argv[2]
+ckb_rpc_url = sys.argv[3]
+
+def send_jsonrpc(url, method, params=[]):
+    payload = {
+        "id": 0,
+        "jsonrpc": "2.0",
+        "method": method,
+        "params": params,
+    }
+    cmd = "curl -s -H 'content-type: application/json' -d '{}' {}".format(json.dumps(payload), url)
+    output = subprocess.check_output(cmd, shell=True).strip().decode("utf-8")
+    resp = json.loads(output)
+    print('[Polyjuice request]: method={}, params={}'.format(method, params))
+    print('[Polyjuice response]: {}'.format(resp))
+    if "error" in resp:
+        error = resp["error"]
+        raise JSONRPCError(message=error['message'], code=error['code'], status_code=400)
+    return resp["result"]
+
+@app.before_request
+def before():
+    print('[method]: {}'.format(request.method))
+    print('[path]: {}'.format(request.path))
+    print('[data]: {}'.format(request.data))
+
+
+@app.after_request
+def after(resp):
+    print('[Response]: {}'.format(resp.data.decode('utf-8')))
+    return resp
+
+
+@jsonrpc.method('eth_getLogs')
+def get_logs() -> str:
+    return 'get_logs'
+
+@jsonrpc.method('eth_call')
+def call(program: Dict, tag: Union[int, str]) -> str:
+    destination = program['to'].lower()
+    data = program['data']
+    return send_jsonrpc(polyjuice_rpc_url, 'static_call', [sender, destination, data])['return_data']
+
+@jsonrpc.method('eth_getCode')
+def get_code(address: str, tag: Union[int, str]) -> str:
+    address = address.lower()
+    print(address, tag)
+    if address == '0x0000000000000000000000000000000000000000':
+        return '0x'
+    else:
+        return send_jsonrpc(polyjuice_rpc_url, 'get_code', [address])['code']
+
+@jsonrpc.method('eth_blockNumber')
+def tip_number() -> str:
+    return send_jsonrpc(ckb_rpc_url, 'get_tip_block_number')
+
+@jsonrpc.method('eth_getBlockByNumber')
+def get_block_by_number(number: str, full_tx: bool) -> Dict[Any, Any]:
+    #     "result": {
+    #   "difficulty": "0x4ea3f27bc",
+    #   "extraData": "0x476574682f4c5649562f76312e302e302f6c696e75782f676f312e342e32",
+    #   "gasLimit": "0x1388",
+    #   "gasUsed": "0x0",
+    #   "hash": "0xdc0818cf78f21a8e70579cb46a43643f78291264dda342ae31049421c82d21ae",
+    #   "logsBloom": "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+    #   "miner": "0xbb7b8287f3f0a933474a79eae42cbca977791171",
+    #   "mixHash": "0x4fffe9ae21f1c9e15207b1f472d5bbdd68c9595d461666602f2be20daf5e7843",
+    #   "nonce": "0x689056015818adbe",
+    #   "number": "0x1b4",
+    #   "parentHash": "0xe99e022112df268087ea7eafaf4790497fd21dbeeb6bd7a1721df161a6657a54",
+    #   "receiptsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+    #   "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+    #   "size": "0x220",
+    #   "stateRoot": "0xddc8b0234c2e0cad087c8b389aa7ef01f7d79b2570bccb77ce48648aa61c904d",
+    #   "timestamp": "0x55ba467c",
+    #   "totalDifficulty": "0x78ed983323d",
+    #   "transactions": [
+    #   ],
+    #   "transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+    #   "uncles": [
+    #   ]
+    # }
+
+    # header:
+    #   compact_target: 0x20010000
+    #   dao: 0x182a95d5921fa12eebf62fa6f286230007eb70034b0000000040025e7011ff06
+    #   epoch: 0x3e80005000000
+    #   hash: 0x066e3314ad9056af1883c43cd6f634ca3074a3b1bbe4adabedb5e96634f81efe
+    #   nonce: 0x7ff7eefe9134c73fda44d18119c8080f
+    #   number: 0x5
+    #   parent_hash: 0xebfd106232606fc008d282da1c09b03e30948c89f1a16ed924fb821ba7cb0334
+    #   proposals_hash: 0x0000000000000000000000000000000000000000000000000000000000000000
+    #   timestamp: 0x17393a79c78
+    #   transactions_root: 0x6aac9ba76ca3ae47ed0528aebd6d3caa65226f340cab6f10955ccdf12ed24bbb
+    #   uncles_hash: 0x0000000000000000000000000000000000000000000000000000000000000000
+    #   version: 0x0
+
+    result = send_jsonrpc(ckb_rpc_url, 'get_block_by_number', [number])
+    header = result['header']
+    return {
+        'difficulty': '0xff',
+        'number': header['number'],
+        'timestamp': hex(int(int(header['timestamp'], 0) / 1000)),
+        'transactions': [],
+        "transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+        'uncles': [],
+    }
+
+@jsonrpc.method('eth_accounts')
+def accounts() -> List[str]:
+    return ['0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7']
+
+@jsonrpc.method('net_version')
+def version() -> str:
+    return '3'
+
+
+if __name__ == '__main__':
+    port = int(sys.argv[1])
+    print('[polyjuice url]: {}'.format(polyjuice_rpc_url))
+    app.run(host='0.0.0.0', port=port, debug=True)
