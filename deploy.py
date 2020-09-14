2d7c3695bf46cf395234d6cf2e4c15222b86feac
Linfeng Qian <thewawar@gmail.com>
2020-09-14 16:32:49 +0800

    Upgrade deploy script

--- deploy.py
+++ deploy.py
@@ -11,7 +11,7 @@ from namehash import namehash, sha3
 
 SENDER1 = "0xc8328aabcd9b9e8e64fbc566c4385c3bdeb219d7"
 SENDER1_PRIVKEY = "d00c06bfd800d27397002dca6fb0993d5ba6399b4238b2f29ee9deb97593d2bc"
-
+eoa_accounts = {}
 
 def send_jsonrpc(url, method, params=[]):
     payload = {
@@ -27,26 +27,30 @@ def send_jsonrpc(url, method, params=[]):
         raise ValueError("JSONRPC ERROR: {}".format(resp["error"]))
     return resp["result"]
 
-def create_contract(url, binary, constructor_args="", sender=SENDER1):
+def create_contract(url, binary, constructor_args="", sender=SENDER1, account_index=0, value=0):
+    eoa_account = eoa_accounts[sender][account_index]
     print("[create contract]:")
     print("  sender = {}".format(sender))
+    print("  account = {}".format(eoa_account))
     print("  binary = 0x{}".format(binary))
     print("    args = 0x{}".format(constructor_args))
-    result = send_jsonrpc(url, "create", [sender, "0x{}{}".format(binary, constructor_args)])
+    result = send_jsonrpc(url, "create", [eoa_account, "0x{}{}".format(binary, constructor_args), value])
     print("  >> created address = {}".format(result["entrance_contract"]))
     return result
 
-def call_contract(url, contract_address, args, is_static=False, sender=SENDER1):
+def call_contract(url, contract_address, args, is_static=False, sender=SENDER1, account_index=0, value=0):
+    eoa_account = eoa_accounts[sender][account_index]
     method = "static_call" if is_static else "call"
     print("[{} contract]:".format(method))
     print("   sender = {}".format(sender))
+    print("  account = {}".format(eoa_account))
     print("  address = {}".format(contract_address))
     print("     args = {}".format(args))
-    return send_jsonrpc(url, method, [sender, contract_address, args])
+    return send_jsonrpc(url, method, [eoa_account, contract_address, args, value])
 
-def mine_blocks(ckb_bin_path, ckb_dir, n=5):
+def mine_blocks(ckb_bin_path, ckb_dir, n=4):
     run_cmd("{} miner -C {} -l {}".format(ckb_bin_path, ckb_dir, n))
-    time.sleep(0.5)
+    time.sleep(1)
 
 def run_cmd(cmd, print_output=True):
     print("[RUN]: {}".format(cmd))
@@ -55,7 +59,7 @@ def run_cmd(cmd, print_output=True):
         print("[Output]: {}".format(output))
     return output
 
-def commit_tx(target_dir, ckb_bin_path, ckb_dir, ckb_rpc_url, privkey_path, result, action_name):
+def commit_tx(target_dir, ckb_bin_path, ckb_dir, ckb_rpc_url, privkey_path, result, polyjuice_rpc_url, action_name):
     result_path = os.path.join(target_dir, "{}.json".format(action_name))
     with open(result_path, "w") as f:
         json.dump(result, f, indent=4)
@@ -74,6 +78,22 @@ def commit_tx(target_dir, ckb_bin_path, ckb_dir, ckb_rpc_url, privkey_path, resu
             print("Transaction sent: {}".format(tx_hash))
             break;
         print("Retry send transaction: {}".format(retry))
+
+    contract_address = result["entrance_contract"]
+    tx_hash = result["tx_hash"]
+    for retry in range(10):
+        current_tx_hash = None
+        try:
+            change = send_jsonrpc(polyjuice_rpc_url, "get_change", [contract_address, None])
+            current_tx_hash = change["tx_hash"]
+        except ValueError:
+            pass
+        if current_tx_hash != tx_hash:
+            time.sleep(0.5)
+            print("Waiting for polyjuice to index transaction: {}, retry: {} ...".format(tx_hash, retry))
+        else:
+            print("Transaction indexed: {}".format(tx_hash))
+            break
 
 def get_code(path):
     with open(path) as f:
@@ -96,7 +116,9 @@ def call(target_dir, ckb_bin_path, ckb_rpc_url, polyjuice_rpc_url, ckb_dir, priv
     result = call_contract(polyjuice_rpc_url, addr, args)
     commit_tx(
         target_dir, ckb_bin_path, ckb_dir, ckb_rpc_url, privkey_path,
-        result, 'call-{}-{}'.format(name, args)[:45],
+        result,
+        polyjuice_rpc_url,
+        'call-{}-{}'.format(name, args)[:45],
     )
     return result
 
@@ -104,7 +126,9 @@ def main(target_dir, ckb_bin_path, ckb_rpc_url, polyjuice_rpc_url, ckb_dir, priv
     def local_commit_tx(result, action_name):
         commit_tx(
             target_dir, ckb_bin_path, ckb_dir, ckb_rpc_url, privkey1_path,
-            result, action_name,
+            result,
+            polyjuice_rpc_url,
+            action_name,
         )
 
     def local_call(addr, fn_name, args, name):
@@ -112,6 +136,8 @@ def main(target_dir, ckb_bin_path, ckb_rpc_url, polyjuice_rpc_url, ckb_dir, priv
             target_dir, ckb_bin_path, ckb_rpc_url, polyjuice_rpc_url, ckb_dir, privkey1_path,
             addr, fn_name, args, name,
         )
+
+    gen_eoa_accounts(ckb_bin_path, ckb_dir, ckb_rpc_url, SENDER1, privkey1_path)
 
     tld_label = gen_label(b'eth')
     resolver_label = gen_label(b'resolver')
@@ -161,7 +187,7 @@ def main(target_dir, ckb_bin_path, ckb_rpc_url, polyjuice_rpc_url, ckb_dir, priv
     fn_set_interface = 'e59d895d'
     zero_node = '0000000000000000000000000000000000000000000000000000000000000000'
 
-    account0 = SENDER1
+    account0 = eoa_accounts[SENDER1][0]
     ## Setup Resolver
     for (base_node, label, node) in [(zero_node, resolver_label, resolver_node),
                                      (zero_node, tld_label, tld_node),
@@ -243,6 +269,9 @@ def main(target_dir, ckb_bin_path, ckb_rpc_url, polyjuice_rpc_url, ckb_dir, priv
         'setSubnodeOwner',
     )
 
+    print('==================================================================')
+    print('[EoA lock args]       : {}'.format(SENDER1))
+    print('[EoA account address] : {}'.format(eoa_accounts[SENDER1][0]))
     print('========================================================================')
     print('ENSRegistry                 : {}'.format(ens_addr))
     print('PublicResolver              : {}'.format(public_resolver_addr))
@@ -252,7 +281,23 @@ def main(target_dir, ckb_bin_path, ckb_rpc_url, polyjuice_rpc_url, ckb_dir, priv
     print('Controller                  : {}'.format(controller_addr))
     print('========================================================================')
 
+
+def gen_eoa_accounts(ckb_bin_path, ckb_dir, ckb_rpc_url, sender, privkey_path):
+    balance = "1000.0"
+    output = run_cmd("polyjuice new-eoa-account --url {} -k {} --balance {}".format(
+        ckb_rpc_url, privkey_path, balance,
+    ))
+    eoa_address = output.strip().splitlines()[-1]
+    mine_blocks(ckb_bin_path, ckb_dir)
+    if sender not in eoa_accounts:
+        eoa_accounts[sender] = []
+    eoa_accounts[sender].append(eoa_address)
+
+
 if __name__ == '__main__':
+    if len(sys.argv) < 5:
+        print('USAGE:\n  python3 deploy.py <tmp-dir> <ckb-binary-path> <ckb-rpc-url> <polyjuice-rpc-url>')
+        exit(-1)
     target_dir = sys.argv[1]
     ckb_bin_path = sys.argv[2]
     ckb_rpc_url = sys.argv[3]
